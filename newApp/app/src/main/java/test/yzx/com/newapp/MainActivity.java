043d76aa427831de46b960ffa31157ee7454e719
yzx <yzxandroid981@163.com>
2018-05-11 22:12:38 +0800

    数据拉去功能

--- newApp/app/src/main/java/test/yzx/com/newapp/MainActivity.java
+++ newApp/app/src/main/java/test/yzx/com/newapp/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //dsjakdhjksahdjksahdjksa
+        //dsjakdhjksahdjksahdjksa
+        //dsjakdhjksahdjksahdjksa
+        //dsjakdhjksahdjksahdjksa
+        //dsjakdhjksahdjksahdjksa
     }
 }
